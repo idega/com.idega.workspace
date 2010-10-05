20324cfb0ce0a978ca55fbd83c2d906ee7f1b44f
Valdas Žemaitis <valdas@idega.com>
2010-10-05 15:48:08 +0300

    Fixed a bug which prevented to log off from ePlatform when logoff link clicked.

--- resources/javascript/workspaceLogin.js
+++ resources/javascript/workspaceLogin.js
@@ -2,8 +2,15 @@ jQuery.noConflict();
 
 jQuery(document).ready(function() {
 	jQuery(".workspaceLogin a").click(function() {
-		jQuery(".workspaceLogin input[name='login_state']").val('logoff');
-		jQuery(this).parents('form').submit();
+		jQuery(".workspaceLogin input[name='login_state']").each(function() {
+			jQuery(this).attr('value', 'logoff');
+		});
+		
+		var form = jQuery(this).parents('form');
+		if (form.attr('enctype') != 'application/x-www-form-urlencoded') {
+			form.attr('enctype', 'application/x-www-form-urlencoded');
+		}
+		form.submit();
 		return false;
 	});
 });
