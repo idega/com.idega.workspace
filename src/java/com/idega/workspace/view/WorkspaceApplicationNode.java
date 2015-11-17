a2f6ce4514a77d8ddbb33552469ed6d4bd11e133
Valdas Žemaitis <valdas@idega.com>
2015-11-17 09:27:08 +0200

    Replacing JSPs with facelets

--- src/java/com/idega/workspace/view/WorkspaceApplicationNode.java
+++ src/java/com/idega/workspace/view/WorkspaceApplicationNode.java
@@ -1,9 +1,10 @@
 /**
- * 
+ *
  */
 package com.idega.workspace.view;
 
 import java.util.Collection;
+
 import com.idega.core.view.ApplicationViewNode;
 import com.idega.core.view.ViewNode;
 import com.idega.idegaweb.IWBundle;
@@ -12,11 +13,8 @@ import com.idega.util.CoreConstants;
 
 
 /**
- * <p>
- * TODO tryggvil Describe Type WorkspaceApplicationNode
- * </p>
  *  Last modified: $Date: 2008/01/24 17:04:44 $ by $Author: valdas $
- * 
+ *
  * @author <a href="mailto:tryggvil@idega.com">tryggvil</a>
  * @version $Revision: 1.3 $
  */
@@ -26,13 +24,13 @@ public class WorkspaceApplicationNode extends ApplicationViewNode {
 	 * @param viewId
 	 * @param parent
 	 */
-	public WorkspaceApplicationNode(String viewId, ViewNode parent,Collection roles) {
+	public WorkspaceApplicationNode(String viewId, ViewNode parent, Collection<String> roles) {
 		super(viewId, parent);
-		
+
 		IWMainApplication iwma = getIWMainApplication();
 		IWBundle workspaceBundle = iwma.getBundle(CoreConstants.WORKSPACE_BUNDLE_IDENTIFIER);
-		setJspUri(workspaceBundle.getJSPURI("workspace.jsp"));
+		setFaceletUri(workspaceBundle.getFaceletURI("workspace.xhtml"));
 		setAuthorizedRoles(roles);
-		
+
 	}
 }
